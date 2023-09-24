3310d991f9706e048b00f33319b77ab3d3df8926
Giga Amiridze <gigaamiridze@gmail.com>
2023-09-24 23:08:32 +0400

    Loaded fonts using expo-font package

--- App.tsx
+++ App.tsx
@@ -1,7 +1,28 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
-export default function App() {
+function App() {
+  const [fontsLoaded] = useFonts({
+    InterLight: require('./assets/fonts/Inter-Light.ttf'),
+    InterRegular: require('./assets/fonts/Inter-Regular.ttf'),
+    InterMedium: require('./assets/fonts/Inter-Medium.ttf'),
+    InterSemiBold: require('./assets/fonts/Inter-SemiBold.ttf'),
+    InterBold: require('./assets/fonts/Inter-Bold.ttf'),
+  });
+
+  useEffect(() => {
+    SplashScreen.preventAutoHideAsync().then(() => {
+      if (fontsLoaded) {
+        SplashScreen.hideAsync();  
+      } 
+    });
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -9,6 +30,8 @@ export default function App() {
     </View>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
