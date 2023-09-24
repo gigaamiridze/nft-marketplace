99e6f219d012b866b90faadc9dc507bd7a17a820
Giga Amiridze <gigaamiridze@gmail.com>
2023-09-25 02:26:17 +0400

    Added Home and Details screen components

---
+++ screens/Details.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+function Details() {
+  return (
+    <SafeAreaView>
+      <Text>Details</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Details;
