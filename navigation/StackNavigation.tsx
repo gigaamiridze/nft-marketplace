3306b012737b1f25218a38e9a7d8e672df564dcc
Giga Amiridze <gigaamiridze@gmail.com>
2023-09-25 02:42:47 +0400

    Added constants for routes

--- navigation/StackNavigation.tsx
+++ navigation/StackNavigation.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Home, Details } from '../screens';
+import { Routes } from '../constants';
 
 const Stack = createStackNavigator();
 
 function StackNavigation() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name='Home' component={Home} />
-      <Stack.Screen name='Details' component={Details} />
+      <Stack.Screen name={Routes.Home} component={Home} />
+      <Stack.Screen name={Routes.Details} component={Details} />
     </Stack.Navigator>
   )
 }
