a4cdb1c448d8e8f28ff2bafd8644fc4805cbb890
Giga Amiridze <gigaamiridze@gmail.com>
2023-09-24 23:16:18 +0400

    Added constants for fonts

---
+++ constants/Fonts.ts
@@ -0,0 +1,7 @@
+export enum Fonts {
+  InterLight = 'InterLight',
+  InterRegular = 'InterRegular',
+  InterMedium = 'InterMedium',
+  InterSemiBold = 'InterSemiBold',
+  InterBold = 'InterBold',
+}
