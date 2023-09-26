ca5dd0329044e5b50a33abd4be468f26d08dca2f
Giga Amiridze <gigaamiridze@gmail.com>
2023-09-26 12:57:09 +0400

    Added colors enum

---
+++ enums/Colors.ts
@@ -0,0 +1,6 @@
+export enum Colors {
+  PRIMARY = '#001F2D',
+  SECONDARY = '#4D626C',
+  WHITE = '#FFFFFF',
+  GRAY = '#74858C',
+}
--- enums/index.ts
+++ enums/index.ts
@@ -1,2 +1,3 @@
+export * from './Colors';
 export * from './Routes';
 export * from './Fonts';
