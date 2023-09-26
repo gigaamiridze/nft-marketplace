6c8f7259ce7d858ae2772b9e9a1da7fbe94e97e9
Giga Amiridze <gigaamiridze@gmail.com>
2023-09-26 12:46:14 +0400

    Added constants for icons

---
+++ constants/icons.ts
@@ -0,0 +1,13 @@
+import search from '../assets/icons/search.png';
+import badge from '../assets/icons/badge.png';
+import heart from '../assets/icons/heart.png';
+import left from '../assets/icons/left.png';
+import eth from '../assets/icons/eth.png';
+
+export default { 
+  search,
+  badge,
+  heart,
+  left,
+  eth 
+};
