78c3e36bd045b033fcb41fdea75193b3b7a63962
Giga Amiridze <gigaamiridze@gmail.com>
2023-09-29 15:16:02 +0400

    Added content for EndDate component

--- components/SubInfo/EndDate.tsx
+++ components/SubInfo/EndDate.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { Heading  } from '../../components';
+import { Colors } from '../../enums';
+import { styles } from './styles';
 
 function EndDate() {
   return (
-    <View>
-      <Text>EndDate</Text>
+    <View style={styles.endDateContainer}>
+      <Heading 
+        title='Ending in' 
+        type={5} 
+        color={Colors.SECONDARY}
+        fontWeight='regular'
+      />
+      <Heading 
+        title='12h 30m' 
+        type={3} 
+      />
     </View>
   )
 }
--- components/SubInfo/styles.ts
+++ components/SubInfo/styles.ts
@@ -1,4 +1,6 @@
 import { StyleSheet } from 'react-native';
+import { shadows } from '../../constants';
+import { Colors, Sizes } from '../../enums';
 
 export const styles = StyleSheet.create({
   infoContainer: {
@@ -9,6 +11,16 @@ export const styles = StyleSheet.create({
   peopleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
+  },
+  endDateContainer: {
+    backgroundColor: Colors.WHITE,
+    paddingVertical: Sizes.X_SMALL,
+    paddingHorizontal: Sizes.REGULAR,
+    borderRadius: Sizes.SMALL,
+    alignItems: 'center',
+    justifyContent: 'center',
+    ...shadows.light,
+    elevation: 1,
   },
   personImage: {
     width: 48,
