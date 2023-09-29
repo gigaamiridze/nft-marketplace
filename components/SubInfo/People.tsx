27d420a73b78a6ab66d137317f9557381b94cfa0
Giga Amiridze <gigaamiridze@gmail.com>
2023-09-29 14:36:59 +0400

    Added content for People component

--- components/SubInfo/People.tsx
+++ components/SubInfo/People.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Image } from 'react-native';
+import { images } from '../../constants';
+import { Sizes } from '../../enums';
+import { styles } from './styles';
 
 function People() {
+  const people = [images.person02, images.person03, images.person04];
+
   return (
-    <View>
-      <Text>People</Text>
+    <View style={styles.peopleContainer}>
+      {people.map((imgUrl, index) => (
+        <Image 
+          key={`person - ${index}`} 
+          source={imgUrl}
+          resizeMode='contain'
+          style={[styles.personImage, { marginLeft: index === 0 ? 0 : -Sizes.SMALL }]}
+        />
+      ))}
     </View>
   )
 }
